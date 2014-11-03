311617c31070a9bff7c38fefc2edefdb37faba69
Christian Deussen <chrisudeussen@gmail.com>
2014-11-03 22:24:23 +0100

    fix graph generator

--- aufgabe1/stresstest.sh
+++ aufgabe1/stresstest.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 burnP6 &
-`sleep 30m && killall burnP6` &
+`sleep 5h && killall burnP6` &
 
 while [ `pgrep burnP6 &>/dev/null` ]
 do
 	printf "%s %s\n" "`date +%d/%m/%Y\ %H:%M:%S`" "`sensors |   grep C | awk -F'[:|(]' '{print $2}'  | grep -o '[0-9]*\.[0-9]' | cut -f1 | xargs`"
-	sleep 3m
+	sleep 18s
 done
