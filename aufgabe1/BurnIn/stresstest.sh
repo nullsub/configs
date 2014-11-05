584021c3bdc45c4aa904d3564735f1c27e15558a
Christian Deussen <chrisudeussen@gmail.com>
2014-11-06 00:31:22 +0100

    add final stresstest data

--- aufgabe1/BurnIn/stresstest.sh
+++ aufgabe1/BurnIn/stresstest.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 burnP6 &
-`sleep 5h && killall burnP6` &
+`sleep 24h && killall burnP6` &
 
 while [ `pgrep burnP6 &>/dev/null` ]
 do
 	printf "%s %s\n" "`date +%d/%m/%Y\ %H:%M:%S`" "`sensors |   grep C | awk -F'[:|(]' '{print $2}'  | grep -o '[0-9]*\.[0-9]' | cut -f1 | xargs`"
-	sleep 18s
+	sleep 80s
 done
